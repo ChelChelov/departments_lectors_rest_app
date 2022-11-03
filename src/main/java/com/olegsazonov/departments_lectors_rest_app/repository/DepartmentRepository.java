c31f04bdf72ce7bdba94bc6b5ecc0c7469425df1
Oleg Sazonov <sazonov1994@gmail.com>
2022-11-03 19:59:36 +0200

    Added spring-boot-starter-data-rest to pom

--- src/main/java/com/olegsazonov/departments_lectors_rest_app/repository/DepartmentRepository.java
+++ src/main/java/com/olegsazonov/departments_lectors_rest_app/repository/DepartmentRepository.java
@@ -2,6 +2,8 @@ package com.olegsazonov.departments_lectors_rest_app.repository;
 
 import com.olegsazonov.departments_lectors_rest_app.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
--- src/main/java/com/olegsazonov/departments_lectors_rest_app/repository/LectorRepository.java
+++ src/main/java/com/olegsazonov/departments_lectors_rest_app/repository/LectorRepository.java
@@ -2,6 +2,8 @@ package com.olegsazonov.departments_lectors_rest_app.repository;
 
 import com.olegsazonov.departments_lectors_rest_app.entity.Lector;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LectorRepository extends JpaRepository<Lector, Long> {
 }
